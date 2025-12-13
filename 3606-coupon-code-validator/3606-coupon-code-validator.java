bc6a8f683bd43706b5b2fd2648f9ebd2590b3616
VallabhiGoyal <vallabhigoyal9@gmail.com>
2025-12-13 13:12:47 +0530

    Time: 19 ms (42.73%), Space: 48.1 MB (11.82%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        
+        Map<String,Integer> priority = new HashMap<>();
+        priority.put("electronics",0);
+        priority.put("grocery",1);
+        priority.put("pharmacy",2);
+        priority.put("restaurant",3);
+
+        List<int[]> valid = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+
+        for(int i = 0; i<code.length; i++){
+            if(!isActive[i]) continue;
+
+            String bl = businessLine[i].trim();
+            String c = code[i];
+
+            if(!priority.containsKey(bl)) continue;
+            if(c.isEmpty()) continue;
+            if(!c.matches("[a-zA-Z0-9_]+")) continue;
+
+            valid.add(new int[]{i});
+        }
+
+        valid.sort((a,b) -> {
+            int p1 = priority.get(businessLine[a[0]]);
+            int p2 = priority.get(businessLine[b[0]]);
+
+            if(p1 != p2){
+                return p1-p2;
+            }
+
+            return code[a[0]].compareTo(code[b[0]]);
+        });
+
+        for (int[] idx : valid) {
+            result.add(code[idx[0]]);
+        }
+
+        return result;
+
+    }
+}
