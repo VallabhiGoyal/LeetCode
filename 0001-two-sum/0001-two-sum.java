7e808acc8501e15c6abec526508fdd3dc406f90a
VallabhiGoyal <vallabhigoyal9@gmail.com>
2025-12-16 19:38:28 +0530

    Time: 2 ms (99.1%), Space: 47.1 MB (27.44%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,41 +1,15 @@
-import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        for(int i = 0; i<nums.length;i++){
-            for(int j  = i+1; j<nums.length; j++){
-                int key = nums[i] + nums[j];
-                if(key == target){
-                    return new int[]{i,j};
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i<nums.length; i++){
+            int temp = target - nums[i];
+            if(map.containsKey(temp)){
+                return new int[]{i, map.get(temp)}; 
             }
+            map.put(nums[i], i);
         }
-        return new int[]{};
-    }
-}
-public class two{
-    public static void main(String[] args){
-        System.out.println("enter the number of elements in the array");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        int[] arr = new int[n];
-        System.out.println("Input : ");
-        for(int i = 0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("enter the target element");
-        int target = sc.nextInt();
 
-        Solution  sol = new Solution();
-        int[] result = sol.twoSum(arr, target);
-
-        if(result.length == 2){
-            System.out.println(result[0] + "," + result[1]);
-        }else{
-            System.out.println("no solution found");
-        }
-        sc.close();
-
+        return new int[] {-1,-1};
     }
 }
