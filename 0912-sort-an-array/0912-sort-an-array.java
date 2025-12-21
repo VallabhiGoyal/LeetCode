a7fc8fda87b185246aace37bf802b1db35fd3920
VallabhiGoyal <vallabhigoyal9@gmail.com>
2025-12-21 19:11:13 +0530

    Time: 33 ms (31.7%), Space: 86.4 MB (23.32%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int n = nums.length;
+
+        for (int i = n/2 - 1; i >= 0; i--) {
+            downHeap(nums, i, n);
+        }
+
+        for (int end = n - 1; end > 0; end--) {
+            swap(nums, 0, end);      
+            downHeap(nums, 0, end); 
+        }
+
+        return nums;
+    }
+
+    private void swap(int arr[], int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+
+    private int[] downHeap(int[] arr, int i, int heapSize){
+        int largest = i;
+
+        while(true){
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left<heapSize && arr[left]>arr[largest]){
+                largest = left;
+            }
+
+            if(right<heapSize && arr[right]>arr[largest]){
+                largest = right;
+            }
+
+            if(largest == i) break;
+
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            i = largest;
+        }
+
+        return arr;
+
+    }
+}
