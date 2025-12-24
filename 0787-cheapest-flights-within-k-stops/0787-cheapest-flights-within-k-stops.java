847fa29cc247ff75d02bf321093b217ad8b88a09
VallabhiGoyal <vallabhigoyal9@gmail.com>
2025-12-24 21:02:57 +0530

    Time: 5 ms (98.33%), Space: 46.6 MB (29.78%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,84 +1,35 @@
 class Solution {
 
-    List<int[]>[] graph;
-
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        createGraph(flights, n);
-        int ans = DijkstraAlgo(src, dst, k, n);
-        return ans == Integer.MAX_VALUE ? -1: ans;
-    }
 
-    public int DijkstraAlgo(int src, int dest, int k, int n){
-        
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(src, 0, 0));
+        int dist[] = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
-        int[][] dist = new int[n][k+2];
+        dist[src] = 0;
 
-        for(int i = 0; i<n; i++){
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        
-        dist[src][0] = 0;
+        for(int i = 0; i<=k; i++){
 
-        while(!pq.isEmpty()){
+            int[] temp = new int[n];
+            for(int j = 0; j<n; j++){
+                temp[j] = dist[j];
+            }
 
-            Pair curr = pq.remove();
-            int node = curr.node;
-            int currCost = curr.cost;
-            int stops = curr.stops;
-            
-            if (node == dest) return currCost;
-            if (stops > k) continue;
+            for(int[] f : flights){
+                int u = f[0];
+                int v = f[1];
+                int wt = f[2];
 
-            for(int[] edge : graph[node]){
+                if (dist[u] == Integer.MAX_VALUE) continue;
 
-                int neigh = edge[0];
-                int wt = edge[1];
-
-                int newCost = currCost + wt;
-
-                if(newCost < dist[neigh][stops+1]){ // relaxation
-                    dist[neigh][stops+1] = newCost;
-                    pq.add(new Pair(neigh, newCost, stops+1));
+                if (dist[u] + wt < temp[v]) {
+                    temp[v] = dist[u] + wt;
                 }
             }
-            
+
+            dist = temp;
         }
 
-        return Integer.MAX_VALUE;
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
     }
 
-    public void createGraph(int[][] flights, int n){
-        graph = new ArrayList[n];
-
-        for(int i = 0; i<n; i++){
-            graph[i] = new ArrayList<>();
-        }
-
-        for(int i = 0; i<flights.length; i++){
-            int src = flights[i][0];
-            int dest = flights[i][1];
-            int cost = flights[i][2];
-
-            graph[src].add(new int[]{dest,cost});
-        }
-    }
-
-    public static class Pair implements Comparable<Pair>{
-        int node;
-        int cost;
-        int stops;
-
-        public Pair(int node, int cost, int stops){
-            this.node = node;
-            this.cost = cost;
-            this.stops = stops;
-        }
-        @Override
-        public int compareTo(Pair other){
-            return this.cost - other.cost;
-        }
-
-    }
 }
