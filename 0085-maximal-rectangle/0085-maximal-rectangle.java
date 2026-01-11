de29d80b4b5330e57fd652117d89b35f1909be24
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-01-11 21:00:31 +0530

    Time: 15 ms (59.33%), Space: 50.7 MB (7.04%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,40 +1,47 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
-
+        int maxRectangle = Integer.MIN_VALUE;
         int rows = matrix.length;
         int cols = matrix[0].length;
 
         int[] heights = new int[cols];
 
-        int maxArea = 0;
         for(int i = 0; i<rows; i++){
             Stack<Integer> stack = new Stack<>();
             for(int j = 0; j<cols; j++){
-                if(matrix[i][j] == '1'){
-                    heights[j]++;
-                }else heights[j] = 0;
-                
-                if(stack.isEmpty() || heights[j]>heights[stack.peek()]){
-                        stack.push(j);
-                }else{
-                    int poppedIndex = 0;
-                    while(!stack.isEmpty() && heights[j]<=heights[stack.peek()]){
-                        poppedIndex = stack.pop();
-                        int height = heights[poppedIndex];
-                        int width = stack.isEmpty()? j : j - stack.peek() - 1;
-                        maxArea = Math.max(maxArea, width*height);
-                    }
-                    stack.push(j);
+                if(matrix[i][j] == '1') heights[j]++;
+                else heights[j] = 0;    
+            }
+            maxRectangle = Math.max(maxRectangle, getMaxArea(heights, stack));
+        }
+        return maxRectangle;
+    }
+
+    private int getMaxArea(int[] heights, Stack<Integer> stack){
+        stack.clear();
+
+        int maxArea = Integer.MIN_VALUE;
+
+        for(int j = 0; j<heights.length; j++){
+            if(stack.isEmpty() || heights[j]>heights[stack.peek()]){
+                stack.push(j);
+            }else{
+                int poppedIndex = 0;
+                while(!stack.isEmpty() && heights[j]<=heights[stack.peek()]){
+                    poppedIndex = stack.pop();
+                    int height = heights[poppedIndex];
+                    int width = stack.isEmpty()? j : j - stack.peek() - 1;
+                    maxArea = Math.max(maxArea, width*height);
                 }
+                stack.push(j);
             }
-            while(!stack.isEmpty()){
-                int poppedIndex = stack.pop();
-                int height = heights[poppedIndex];
-                int width = stack.isEmpty()? cols : cols - stack.peek() - 1;
-                maxArea = Math.max(maxArea, width*height);
-            }
-            
+        }
+
+        while(!stack.isEmpty()){
+            int poppedIndex = stack.pop();
+            int height = heights[poppedIndex];
+            int width = stack.isEmpty()? heights.length : heights.length - stack.peek() - 1;
+            maxArea = Math.max(maxArea, width*height);
         }
 
         return maxArea;
