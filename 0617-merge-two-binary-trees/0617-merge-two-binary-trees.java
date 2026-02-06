badcc26689e3d16834fe59e7647a04dea4639db8
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-02-06 07:11:55 +0530

    Time: 0 ms (100%), Space: 47.1 MB (48.82%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -25,9 +25,7 @@ class Solution {
         if(root1 == null) return root2;
         if(root2 == null) return root1;
 
-        if(root1 != null && root2 != null){
-            root1.val += root2.val;
-        }
+        root1.val += root2.val;
 
         root1.left = helper(root1.left, root2.left);
         root1.right = helper(root1.right, root2.right);
