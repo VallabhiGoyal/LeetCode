523c070e2ff67d69d170aeb481a7380ad0d1fd06
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-02-21 06:49:42 +0530

    Time: 3 ms (88.51%), Space: 48.4 MB (43.23%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -3,40 +3,15 @@ class Solution {
         int a = matrix.length;
         int b = matrix[0].length;
 
-        int colIdx = -1;
+        int row = 0;
+        int col = b-1;
         
-        for(int j = 0; j<b-1; j++){
-            if(matrix[0][j] == target) return true;
-            if(matrix[0][j]<target && matrix[0][j+1]>target){
-                colIdx = j;
-                break;
-            }
-        }
-        
-        if(colIdx == -1 && matrix[0][b-1]<=target) colIdx = b-1;
-
-        if(colIdx == -1) return false;
-
-        int rowIdx = -1;
-        for(int i = 0; i<a-1; i++){
-            if(matrix[i][0] == target) return true;
-            if(matrix[i][0]<target && matrix[i+1][0]>target){
-                rowIdx = i;
-                break;
-            }
-        }
-
-        if(rowIdx == -1 && matrix[a-1][0]<=target) rowIdx = a-1;
-
-        if(rowIdx == -1) return false;
-
-        for(int i = 0; i<=rowIdx; i++){
-            for(int j = 0; j<=colIdx; j++){
-                if(matrix[i][j] == target) return true;
-            }
+        while(row<a && col >= 0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) col--;
+            else row++;
         }
 
         return false;
-
     }
 }
