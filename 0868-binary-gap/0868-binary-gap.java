87f3c2733aa59e0010576f5bf983aeda07cd7f29
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-02-23 05:30:03 +0530

    Time: 1 ms (40.73%), Space: 42.3 MB (48.38%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.java
+++ 0868-binary-gap/0868-binary-gap.java
@@ -2,21 +2,19 @@ class Solution {
     public int binaryGap(int n) {
         int temp  = n;
         String binary = Integer.toBinaryString(n);
-        int diff = 0;
+        int maxDiff = 0;
+        int preIdx = -1;
+        int gap  = 0;
         for(int i = 0; i<binary.length(); i++){
             char ch = binary.charAt(i);
-            if(ch == '1'){
-                int j = i;
-                for(j  = i+1; j<binary.length(); j++){
-                    if(binary.charAt(j) == '1'){
-                        diff = j-i;
-                        break;
-                    }
-                }
-                i = j;
+            if(ch == '1' && preIdx == -1) preIdx = i;
+            else if(ch == '1'){
+                gap = i - preIdx;
+                preIdx = i;
+                if(gap>maxDiff) maxDiff = gap;
             }
         }
 
-        return diff;
+        return maxDiff;
     }
 }
