b2a0e925ed56c0ee1288e1c361b91a6bdc1b0b08
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-02-23 06:06:07 +0530

    Time: 47 ms (88.4%), Space: 69.3 MB (85.93%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -10,12 +10,12 @@ class Solution {
         }
         set.add(curr);
 
+        int mask = total - 1;
         for(int i = 1; i<s.length()-k+1; i++){
-            curr = (curr<<1) | s.charAt(i+k-1);
+            curr = ((curr<<1)&mask) | (s.charAt(i+k-1) - '0');
             set.add(curr);
             if(set.size() == total) return true;
         }
-    
         return false;
     }
 }
