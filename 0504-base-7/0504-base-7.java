8154084245bb620f544ed6a47766e424fbb969c7
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-02-24 05:46:19 +0530

    Time: 3 ms (24.72%), Space: 43 MB (26.33%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,11 +1,11 @@
 class Solution {
     public String convertToBase7(int num) {
         if(num == 0) return "0";
-        String ch = new String();
+        StringBuilder ch = new StringBuilder();
         int temp = Math.abs(num);
         while(temp>0){
             int rem = temp % 7;
-            ch += rem;
+            ch.append(rem);
             temp /= 7;
         }
         String answer = new StringBuilder(ch).reverse().toString();
