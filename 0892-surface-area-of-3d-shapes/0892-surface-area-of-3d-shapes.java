3cb930625a5993116634df28f7b82bf521e78490
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-03-03 10:59:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.java
+++ 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.java
@@ -5,23 +5,31 @@ class Solution {
         int area = 0;
         for(int i = 0; i<n; i++){
             for(int j = 0; j<n; j++){
-                int height = grid[i][j];
-
-                if(height > 0) area += 2;
-
-                if(i>0) area += Math.max(height - grid[i-1][j], 0);
-                else area += height;
-                
-                if(j>0) area += Math.max(height - grid[i][j-1], 0);
-                else area += height;
-                
-                if (i < n - 1) area += Math.max(height - grid[i + 1][j], 0);
-                else area += height;
-                
-                if (j < n - 1) area += Math.max(height - grid[i][j + 1], 0);
-                else area += height;
+                area += getArea(grid, i, j);
             }
         }
+
+        return area;
+    }
+
+    private int getArea(int[][] grid, int i, int j){
+        int area = 0;
+        int n = grid.length;
+        int height = grid[i][j];
+
+        if(height > 0) area += 2;
+
+        if(i>0) area += Math.max(height - grid[i-1][j], 0);
+        else area += height;
+                
+        if(j>0) area += Math.max(height - grid[i][j-1], 0);
+        else area += height;
+                
+        if (i < n - 1) area += Math.max(height - grid[i + 1][j], 0);
+        else area += height;
+
+        if (j < n - 1) area += Math.max(height - grid[i][j + 1], 0);
+        else area += height;
 
         return area;
     }
