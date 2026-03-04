403beb7c3fe8cb58c7dabb2685d6d6170e3c3156
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-03-04 21:29:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -3,13 +3,11 @@ class Solution {
         int n = columnTitle.length();
         if(n == 0) return 0;
 
-        int k = 1;
         int num = 0;
-        for(int i = n -1; i>=0; i--){
+        for(int i = 0; i<n; i++){
             char ch = columnTitle.charAt(i);
             int curr = (int)ch - 64;
-            num = num + curr*k;
-            k *= 26;
+            num = num*26 + curr;
         }
 
         return num;
