8302ac63494ffa2d0a98c3fdcf3973f6747d90ce
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-03-05 06:35:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -3,22 +3,25 @@ class Solution {
         int n = s.length(); 
         if( n == 1) return 0;
 
-        int[] even0 = new int[2];
-        int[] odd0 = new int[2];
+        int even0 = 0;
+        int even1 = 0;
+        int odd0 = 0;
+        int odd1 = 0;
 
         //even 0 odd 1 ---> 01
         for(int i = 0; i<s.length(); i = i + 2){
             char ch = s.charAt(i);
-            if(ch == '0') even0[0]++;
-            else odd0[1]++;
+            if(ch == '0') even0++;
+            else odd1++;
         }
+
         //odd 0 even 1 ---> 10
         for(int i = 1; i<s.length(); i = i+ 2){
             char ch = s.charAt(i);
-            if(ch == '0') odd0[0]++;
-            else even0[1]++;
+            if(ch == '0') odd0++;
+            else even1++;
         }
 
-        return n - Math.max(even0[0]+even0[1], odd0[0] + odd0[1]);
+        return n - Math.max(even0+even1, odd0 + odd1);
     }
 }
