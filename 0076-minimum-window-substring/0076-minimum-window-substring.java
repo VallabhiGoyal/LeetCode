d69b9d62e0b971a9c85c054a8d124833aeb3c4e1
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-03-06 12:20:28 +0530

    Time: 20 ms (39.86%), Space: 48.1 MB (15.62%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        if(n>m) return "";
+
+        Map<Character, Integer> tmap = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            tmap.put(t.charAt(i), tmap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        int i = 0;
+        int count = tmap.size();
+
+
+        int left = -1;
+        int right = -1;
+        int minLen = Integer.MAX_VALUE;
+
+        for(int j = 0; j<m; j++){
+            char ch = s.charAt(j);
+            if(tmap.containsKey(ch)){
+                tmap.put(ch, tmap.get(ch)-1);
+                if(tmap.get(ch) == 0) count--;
+            }
+
+            if(count == 0){
+                while(!tmap.containsKey(s.charAt(i)) || tmap.get(s.charAt(i)) < 0){
+                    char ch2 = s.charAt(i);
+                    if(tmap.containsKey(ch2)){
+                        tmap.put(ch2, tmap.get(ch2) + 1);
+                        if(tmap.get(ch2) > 0) break;
+                    }
+                    i++;
+                }
+                if(minLen > j-i+1){
+                    left = i;
+                    right = j;
+                    minLen = j-i+1;
+                }
+                tmap.put(s.charAt(i), tmap.get(s.charAt(i)) + 1);
+                count++;
+                i++;
+            }
+        }
+        if(left == -1 || right == -1) return "";
+        return s.substring(left, right+1);
+    }
+}
