48309b6ed2eb8a38777f1ed21ea6c410d0b2bd78
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-03-06 19:34:12 +0530

    Time: 2 ms (94.4%), Space: 72.1 MB (53.64%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -13,18 +13,18 @@ class Solution {
             product *= nums[i];
         }
 
-        if(countZ == n) return nums;
+        if(countZ>1){
+            for(int i = 0; i<n; i++){
+                answer[i] = 0;
+            }
+            return answer;
+        }
 
         for(int i = 0; i<n; i++){
             if(countZ > 0){
                 if(nums[i]!=0){
                     answer[i] = 0;
                     continue;
-                }else{
-                    if(countZ>1){
-                         answer[i] = 0;
-                        continue;
-                    }
                 }
             }
             if(nums[i] == 0){
