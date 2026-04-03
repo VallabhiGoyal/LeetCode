010555ad97387c8441783138a2ebac2649eaad4b
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-03 11:31:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -2,25 +2,20 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
+        if(n == 2) return Math.max(nums[0], nums[1]);
+        return Math.max(robRange(nums, 0, n-1), robRange(nums, 1, n));
+    }
 
-        int[] t = new int[n];
+    public int robRange(int[] nums, int start, int end){
+        int t0 = nums[start];
+        int t1 = Math.max(nums[start], nums[start+1]);
 
-        int t0 = nums[0];
-        int t1 = Math.max(nums[0], nums[1]);
-        for(int i = 2; i<n-1; i++){
+        for(int i = start+2; i<end; i++){
             int curr = Math.max(nums[i] + t0, t1);
             t0 = t1;
             t1 = curr;
         }
-        if(n<3) return t1;
-        int t3 = nums[1];
-        int t4 = Math.max(nums[1], nums[2]);
-        for(int i = 3; i<n; i++){
-            int curr = Math.max(nums[i] + t3, t4);
-            t3 = t4;
-            t4 = curr;
-        }
 
-        return Math.max(t1, t4);
+        return t1;
     }
 }
