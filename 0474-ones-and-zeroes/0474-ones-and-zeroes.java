d2b540f4eb64da5cb690878bc67961998ad214a0
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-14 11:10:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -2,25 +2,22 @@ class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
         int len = strs.length;
 
-        int[][] t = new int[len][2];
+        int[][] dp = new int[m+1][n+1];
 
         for(int i = 0; i<len; i++){
             String s = strs[i];
             int size = s.length();
 
+            int zero = 0;
+            int one = 0;
             for(int j = 0; j<size; j++){
-                if(s.charAt(j) == '0') t[i][0]++;
-                else t[i][1]++;
+                if(s.charAt(j) == '0') zero++;
+                else one++;
             }
-        }
-
-        int[][] dp = new int[m+1][n+1];
-
-        for(int i = 0; i<len; i++){
             for(int j = m; j>=0; j--){
                 for(int k = n; k>=0; k--){
-                    if(t[i][0] <= j && t[i][1] <= k){
-                        dp[j][k] = Math.max(dp[j][k], 1 + dp[j - t[i][0]][k - t[i][1]]);
+                    if(zero <= j && one <= k){
+                        dp[j][k] = Math.max(dp[j][k], 1 + dp[j - zero][k - one]);
                     }
                 }
             }
