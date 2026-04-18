52c53a4a1de0c27450cb1a53df0b8d38949b9d00
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-18 12:43:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -3,35 +3,26 @@ class Solution {
         int n = s.length();
 
         if(s.charAt(0) == '0') return 0;
-        if(n == 1) return 1;
 
         int[] dp = new int[n];
         dp[0] = 1;
-        
-        dp[1] = 0;
-        if(s.charAt(1) != '0'){
-            dp[1] += dp[0];
-            if((s.charAt(0) == '1') || (s.charAt(0) == '2' && s.charAt(1) <= '6')) dp[1] += 1;
-        }
-        else{
-            if((s.charAt(0) == '1') || (s.charAt(0) == '2' && s.charAt(1) <= '6')) dp[1] += 1;
-            else return 0;
-        }
 
-        for(int i = 2; i<n; i++){
+        for(int i = 1; i<n; i++){
+            dp[i] = 0;
+
             char curr = s.charAt(i);
             char prev = s.charAt(i-1);
 
             if(curr != '0'){
                 dp[i] += dp[i-1];
-                if((prev == '1') || (prev == '2' && curr <= '6')){
-                    dp[i] += dp[i-2];
-                }
-            }else{
-                if(prev == '1' || prev == '2'){
-                    dp[i] += dp[i-2];
-                }else return 0;
             }
+
+            if(prev == '1' || (prev == '2' && curr <= '6')){
+                if(i >= 2) dp[i] += dp[i-2];
+                else dp[i] += 1;
+            }
+
+            if(dp[i] == 0) return 0;
         }    
 
         return dp[n-1];    
