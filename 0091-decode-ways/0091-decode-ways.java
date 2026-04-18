e78f5982e093a971ee51952f43e5d1f007412d2c
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-18 12:51:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -4,27 +4,30 @@ class Solution {
 
         if(s.charAt(0) == '0') return 0;
 
-        int[] dp = new int[n];
-        dp[0] = 1;
+        int t0 = 1;
+        int t1 = 1;
 
         for(int i = 1; i<n; i++){
-            dp[i] = 0;
+            int ans = 0;
 
             char curr = s.charAt(i);
             char prev = s.charAt(i-1);
 
             if(curr != '0'){
-                dp[i] += dp[i-1];
+                ans += t1;
             }
 
             if(prev == '1' || (prev == '2' && curr <= '6')){
-                if(i >= 2) dp[i] += dp[i-2];
-                else dp[i] += 1;
+                ans += t0;
             }
 
-            if(dp[i] == 0) return 0;
+
+            if(ans == 0) return 0;
+
+            t0 = t1;
+            t1 = ans;
         }    
 
-        return dp[n-1];    
+        return t1;    
     }
 }
