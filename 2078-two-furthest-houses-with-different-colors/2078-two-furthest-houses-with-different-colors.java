b032555150209faeea391452dc15c6c54ffffb04
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-20 22:02:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -4,13 +4,17 @@ class Solution {
 
         int max = 0;
 
-        int j = n-1;
-        for(int i = 0; i<n; i++){
-            while(colors[i] == colors[j]){
-                j--;
+        for(int i = 0; i<n-1; i++){
+            if(colors[i] != colors[n-1]){
+                max = Math.max(max, n-i-1);
             }
-            max = Math.max(max, j-i);
-            j = n-1;
+        }
+
+        for(int i = n-1; i>=1; i--){
+            if(colors[0] != colors[i]){
+                max = Math.max(max, i);
+                break;
+            }
         }
 
         return max;
