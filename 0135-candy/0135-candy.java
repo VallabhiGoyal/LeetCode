19643001749aef5441bdafc9c17b995b9e4cb3f8
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-21 10:57:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -8,26 +8,20 @@ class Solution {
         }
 
         for(int i = 0; i<n; i++){
-            boolean incr = false;
             if(i>0 && ratings[i] > ratings[i-1]){
                 if(temp[i-1] >= temp[i]) temp[i] = temp[i-1]+1;
-                incr = true;
             }
             if(i<n-1 && ratings[i] > ratings[i+1]){
                 if(temp[i+1] >= temp[i]) temp[i] = temp[i+1]+1;
-                incr = true;
             }
         }
 
         for(int i = n-1; i>=0; i--){
-            boolean incr = false;
             if(i>0 && ratings[i] > ratings[i-1]){
                 if(temp[i-1] >= temp[i]) temp[i] = temp[i-1]+1;
-                incr = true;
             }
             if(i<n-1 && ratings[i] > ratings[i+1]){
                 if(temp[i+1] >= temp[i]) temp[i] = temp[i+1]+1;
-                incr = true;
             }
         }
 
