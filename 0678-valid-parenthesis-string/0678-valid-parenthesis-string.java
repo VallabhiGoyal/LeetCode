ce118460b6241b0611d66427d6a05535bc5ef327
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-21 10:28:43 +0530

    Time: 0 ms (100%), Space: 42.8 MB (38.06%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int n = s.length();
+
+        int min = 0;
+        int max = 0;
+
+        for(int i = 0; i<n; i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                min++;
+                max++;
+            }
+            else if(ch == ')'){
+                if(min > 0){
+                    min--;
+                    max--;
+                }else{
+                    if(max>0) max--;
+                    else return false;
+                }
+            }else{
+                if(min > 0) min--;
+                max++;
+            }
+        }
+
+        return min == 0;
+    }
+}
