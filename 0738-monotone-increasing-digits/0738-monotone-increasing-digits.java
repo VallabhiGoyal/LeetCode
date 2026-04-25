43ec790a7c752aa2c1c71834b5d152b4e083b29a
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-25 07:36:45 +0530

    Time: 0 ms (100%), Space: 42.1 MB (81.09%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.java
@@ -1,25 +1,27 @@
 class Solution {
     public int monotoneIncreasingDigits(int n) {
-        int[] digit = new int[10];
 
-        for(int i = 9; i>=0; i--){
-            digit[i] = n % 10;
-            n /= 10;
+        int[] digit = new int[10];
+        for(int i = 9; i >=0; i--){
+            digit[i] = n%10;
+            n/=10;
         }
-        
-        for(int i = 0; i<9; i++){
-            if(digit[i] > digit[i+1]){
-                digit[i]--;
-                for(int j = i+1; j<10; j++){
-                    digit[j] = 9;
-                }
-                i = -1;
+
+        int mark = 10;
+        for(int i = 9; i > 0; i--){
+            if(digit[i] < digit[i - 1]){
+                digit[i - 1]--;
+                mark = i;
             }
         }
 
+        for(int i = mark; i < 10; i++){
+            digit[i] = 9;
+        }
+
         int ans = 0;
-        for(int i = 0; i<10; i++){
-            ans = ans*10 + digit[i];
+        for(int i = 0; i < 10; i++){
+            ans = ans * 10 + digit[i];
         }
 
         return ans;
