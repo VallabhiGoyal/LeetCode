29904d4c2d06b86b6f9425b7c3ef34746510a0df
VallabhiGoyal <vallabhigoyal9@gmail.com>
2026-04-26 09:01:44 +0530

    Time: 18 ms (80.48%), Space: 140.4 MB (39.98%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,55 +1,40 @@
 class Solution {
+    int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+    int m, n;
+
     public boolean containsCycle(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        m = grid.length;
+        n = grid[0].length;
 
         boolean[][] visited = new boolean[m][n];
 
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(!visited[i][j]){
                     if(dfs(grid, visited, i, j, -1, -1)) return true;
                 }
             }
         }
-
         return false;
     }
 
-    private boolean dfs(char[][] grid, boolean[][] visited, int row, int col, int parentRow, int parentCol){
-        int m = grid.length;
-        int n = grid[0].length;
-
+    private boolean dfs(char[][] grid, boolean[][] visited, int row, int col, int pr, int pc){
         visited[row][col] = true;
+        char ch = grid[row][col];
 
-        if(row-1 >= 0 && grid[row][col] == grid[row-1][col]){
-            if(!visited[row-1][col]){
-                if(dfs(grid, visited, row-1, col, row, col)) return true;
-            }           
-            else if(!(row-1 == parentRow && col == parentCol)) return true;
+        for(int[] d : dirs){
+            int nr = row + d[0];
+            int nc = col + d[1];
+
+            if(nr < 0 || nc < 0 || nr >= m || nc >= n) continue;
+            if(grid[nr][nc] != ch) continue;
+
+            if(!visited[nr][nc]){
+                if(dfs(grid, visited, nr, nc, row, col)) return true;
+            } else if(!(nr == pr && nc == pc)){
+                return true;
+            }
         }
-
-        if(col-1 >= 0 && grid[row][col] == grid[row][col-1]){
-            if(!visited[row][col-1]){
-                if(dfs(grid, visited, row, col-1, row, col)) return true;
-            }           
-            else if(!(row == parentRow && col-1 == parentCol)) return true;
-        }
-
-        if(row+1 < m && grid[row][col] == grid[row+1][col]){
-            if(!visited[row+1][col]){
-                if(dfs(grid, visited, row+1, col, row, col)) return true;
-            }           
-            else if(!(row+1 == parentRow && col == parentCol)) return true;
-        }
-
-        if(col + 1 < n && grid[row][col] == grid[row][col+1]){
-            if(!visited[row][col+1]){
-                if(dfs(grid, visited, row, col+1, row, col)) return true;
-            }           
-            else if(!(row == parentRow && col+1 == parentCol)) return true;
-        }
-
         return false;
     }
 }
